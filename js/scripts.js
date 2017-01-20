ca8c1f9ea0dbc416bac193c5988e6f4e034cec7c
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-01-20 09:03:34 -0800

    Add basic DOM manipulation script Declar userInput variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 //Business Logicus
+var userInput;
 
 //User Interface Logicus
+$(function(){
+  $("form").submit(function(event) {
+    event.preventDefault();
+    userInput = parseInt($("#user-input").val());
+    $("ul#output").text(pingPonger(userInput));
+  });
+});
