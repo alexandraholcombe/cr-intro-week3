e0db8f8f94709872cc4c0fa4eea0bad2697de43f
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-01-20 09:59:03 -0800

    Return outputArray

--- js/scripts.js
+++ js/scripts.js
@@ -8,20 +8,19 @@ var pingPonger = function(input) {
   output = 0;
   for (var i = 1; i <= input; i++) {
     if (i % 15 === 0) {
-      output = output + pingPongs[2]
+      output = pingPongs[2]
       outputArray.push(output);
     } else if (i % 5 === 0) {
-      output = output + pingPongs[1]
+      output = pingPongs[1]
       outputArray.push(output);
     } else if (i % 3 === 0) {
-      output = output + pingPongs[0];
+      output = pingPongs[0];
       outputArray.push(output);
     } else {
-      output = output + i + ", "
-      outputArray.push(output);
+      outputArray.push(i);
     };
   };
-  return output;
+  return outputArray;
 };
 
 //User Interface Logicus
