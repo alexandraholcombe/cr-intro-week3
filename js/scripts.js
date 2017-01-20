a42d24fe079a3cb3be5733219180f3b05dff74ee
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-01-20 09:39:59 -0800

    return pong for all numbers divisible by 5

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,11 @@ var output;
 var pingPonger = function(input) {
   output = 0;
   for (var i = 1; i <= input; i++) {
-    if (i % 3 === 0) {
+    if (i % 15 === 0) {
+      output = output + "ping-pong, "
+    } else if (i % 5 === 0) {
+      output = output + "pong, "
+    } else if (i % 3 === 0) {
       output = output + "ping, ";
     } else {
       output = output + i + ", "
