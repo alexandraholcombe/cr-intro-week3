99666f3de8df08d98e0e86d4f56f51d72dc13921
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-01-20 09:52:16 -0800

    Create array pingPongs to house strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
 //Business Logicus
 var userInput;
-var output;
+var outputArray = [];
+var pingPongs = ["ping", "pong", "ping-pong"]
 
 var pingPonger = function(input) {
   output = 0;
   for (var i = 1; i <= input; i++) {
     if (i % 15 === 0) {
-      output = output + "ping-pong, "
+      output = output + pingPongs[2]
     } else if (i % 5 === 0) {
-      output = output + "pong, "
+      output = output + pingPongs[1]
     } else if (i % 3 === 0) {
-      output = output + "ping, ";
+      output = output + pingPongs[0];
     } else {
       output = output + i + ", "
     };
