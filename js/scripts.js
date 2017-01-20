11c740cb9952fba647660934bd9a4b1555a6855f
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-01-20 09:35:52 -0800

    Branch pingPonger function for all numbers divisible by 3

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,12 @@ var output;
 var pingPonger = function(input) {
   output = 0;
   for (var i = 1; i <= input; i++) {
-    output = output + i + ", "
-  }
+    if (i % 3 === 0) {
+      output = output + "ping, ";
+    } else {
+      output = output + i + ", "
+    };
+  };
   return output;
 };
 
